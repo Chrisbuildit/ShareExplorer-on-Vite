d5659e8faeb5167af9cff40af37dea26435dccbe
Chrisbuildit <chris@DESKTOP-92P9M55>
2023-10-25 22:05:31 +0200

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,12 +6,12 @@ function Home() {
 
     return (
         <main className='Glacier'>
-            <body className="Home">
+            <section className="Home">
                 <h3>Welcome on this website</h3>
                 <p>Here you can find the most important data for company shares while enjoying beautiful mountain scenery.</p>
                 <p>Please note that you can only perform - approximately 2 searches per minute - for now.</p>
                 <p>We are working on expanding this.</p>                
-            </body>
+            </section>
         </main>
     );
 }
