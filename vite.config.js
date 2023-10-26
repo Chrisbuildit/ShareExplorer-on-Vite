2b75e19f24c91df4926b1e661a390c398da5198a
Chrisbuildit <chris@DESKTOP-92P9M55>
2023-10-26 21:32:52 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,21 @@
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// const cherryPickedKeys = [
-//   "SOME_KEY_IN_YOUR_ENV_FILE",
-//   "SOME_OTHER_KEY_IN_YOUR_ENV_FILE",
-// ];
+const cherryPickedKeys = [
+  "SOME_KEY_IN_YOUR_ENV_FILE",
+  "SOME_OTHER_KEY_IN_YOUR_ENV_FILE",
+];
 
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-//   const processEnv = {};
-//   cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
 
-//   return {
-//     define: {
-//       'process.env': processEnv
-//     },
-//     server: {
-//       open: true,
-//       hot: true,
-//     },
-//     build: {
-//       outDir: 'build',
-//     },
-//     plugins: [react()],
-//   }
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(() => {
   return {
+    define: {
+      'process.env': processEnv
+    },
     server: {
       open: true,
       hot: true,
@@ -40,5 +24,21 @@ export default defineConfig(() => {
       outDir: 'build',
     },
     plugins: [react()],
-  };
-});
+  }
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(() => {
+//   return {
+//     server: {
+//       open: true,
+//       hot: true,
+//     },
+//     build: {
+//       outDir: 'build',
+//     },
+//     plugins: [react()],
+//   };
+// });
